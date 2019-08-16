91ae50f0c90825cc822882307cc377284205bfcb
Narryel <35465534+Narryel@users.noreply.github.com>
2019-08-16 13:46:14 +0300

    Update Test.java

--- testFramework/src/main/java/ru/otus/testFramework/annotations/Test.java
+++ testFramework/src/main/java/ru/otus/testFramework/annotations/Test.java
@@ -9,5 +9,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target(ElementType.METHOD)
 @Retention(RUNTIME)
 public @interface Test {
+	//дефолтным выбран Exception.class потому что его, вроде, стандартная библиотека не кидает
 	Class expectedException() default Exception.class;
 }
